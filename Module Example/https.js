ab24f9513ed3a7e6a3ec680532f47d867abbd1fc
Fumigatus <alpcan237@gmail.com>
2022-05-03 19:39:53 +0300

    Exporting from modules

--- Module Example/https.js
+++ Module Example/https.js
@@ -1,11 +1,13 @@
-const request = require('./request')
-const response = require('./response')
+const { send, REQUEST_TIMEOUT} = require('./request')
+const { read } = require('./response')
 
 
-function makeRequest(url, data){
-    request.send(url,data)
-    return response.read()
+function makeRequest(url, data) {
+    send(url, data)
+    return read()
 }
 
-const responseData = makeRequest('https://www.google.com','hello');
+const responseData = makeRequest('https://www.google.com', 'hello');
 console.log(responseData)
+
+console.log(REQUEST_TIMEOUT)
--- Module Example/request.js
+++ Module Example/request.js
@@ -1,12 +1,19 @@
-function encrypt(data){
+// module.exports.REQUEST_TIMEOUT=500
+// exports.REQUEST_TIMEOUT = 500
+const REQUEST_TIMEOUT=500
+
+function encrypt(data) {
     return 'encrypted data'
 }
 
-function send(url, data){
-    const encryptedData=encrypt(data)
+// module.exports.send = function send(){....}
+// exports.send = function send() {....}
+function send(url, data) {
+    const encryptedData = encrypt(data)
     console.log(`sending ${encryptedData} to ${url}`)
 }
 
-module.exports={
+module.exports = {
+    REQUEST_TIMEOUT,
     send
 }
