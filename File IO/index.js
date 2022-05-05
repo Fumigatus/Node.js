abfda5e4ab76bef34dde9d7e018946b3218c08bb
Fumigatus <alpcan237@gmail.com>
2022-05-05 21:41:40 +0300

    parsing data

--- File IO/index.js
+++ File IO/index.js
@@ -3,10 +3,14 @@ const { parse } = require('csv-parse')
 const result = []
 const fs = require('fs')
 fs.createReadStream('kepler.csv')
-    .on('data',(data)=>{
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
         result.push(data)
     })
-    .on('error',err=>console.log(err))
-    .on('end',()=>{
+    .on('error', err => console.log(err))
+    .on('end', () => {
         console.log(result)
     })
