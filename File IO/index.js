30abcb160117ce4a60f9b8ceca34c39c0628c048
Fumigatus <alpcan237@gmail.com>
2022-05-05 21:31:03 +0300

    reading file

--- File IO/index.js
+++ File IO/index.js
@@ -1,2 +1,12 @@
 const { parse } = require('csv-parse')
 
+const result = []
+const fs = require('fs')
+fs.createReadStream('kepler.csv')
+    .on('data',(data)=>{
+        result.push(data)
+    })
+    .on('error',err=>console.log(err))
+    .on('end',()=>{
+        console.log(result)
+    })
