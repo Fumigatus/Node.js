d214e774d4614e2ce6a2633e33ef358631dcd9f3
Fumigatus <alpcan237@gmail.com>
2022-05-05 21:58:46 +0300

    finding habitable planets

---
+++ File IO (Planet Project)/index.js
@@ -0,0 +1,28 @@
+const { parse } = require('csv-parse')
+
+const result = []
+const fs = require('fs')
+
+function isHabitable(planet){
+    return planet['koi_disposition']==='CONFIRMED' 
+    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11 
+    && planet['koi_prad'] < 1.6;
+}
+
+fs.createReadStream('kepler.csv')
+    .pipe(parse({
+        comment: '#',
+        columns: true,
+    }))
+    .on('data', (data) => {
+        if (isHabitable(data)){
+            result.push(data)
+        }
+    })
+    .on('error', err => console.log(err))
+    .on('end', () => {
+        console.log('Habitable planets;')
+        for(x of result){
+            console.log(x['kepler_name'])
+        }
+    })
