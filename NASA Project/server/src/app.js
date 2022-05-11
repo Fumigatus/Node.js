d609fafc29d7f93a092ac5087376a201965706a3
Fumigatus <alpcan237@gmail.com>
2022-05-11 05:22:45 +0300

    logging with morgan

--- NASA Project/server/src/app.js
+++ NASA Project/server/src/app.js
@@ -1,6 +1,7 @@
 const PATH = require('path')
 const express = require('express')
 const cors = require('cors')
+const morgan = require('morgan')
 
 const planetsRouter = require('./routes/planets/planets_router')
 
@@ -9,13 +10,15 @@ const app = express()
 app.use(cors({
     origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'))
+
 app.use(express.json())
 app.use(express.static(PATH.join(__dirname, '..', 'public')))
 
 
 app.use(planetsRouter)
 app.get('/', (request, response) => {
-    response.sendFile(PATH.join(__dirname,'..','public','index.html'))
+    response.sendFile(PATH.join(__dirname, '..', 'public', 'index.html'))
 })
 
 
