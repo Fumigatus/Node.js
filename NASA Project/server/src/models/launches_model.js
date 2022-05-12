1531e2e2e94f5e42a2f8788920c82a999079018d
Fumigatus <alpcan237@gmail.com>
2022-05-12 05:36:02 +0300

    post launches creating launch

--- NASA Project/server/src/models/launches_model.js
+++ NASA Project/server/src/models/launches_model.js
@@ -1,10 +1,12 @@
 const launches = new Map()
 
+let lastFlightNumber=100
+
 const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
-    launchData: new Date('December 17 2022'),
+    launchData: new Date('December 17, 2022'),
     target: 'Kepler-442 b',
     customer: ['NASA', 'NOAA'],
     upcoming: true,
@@ -18,6 +20,20 @@ function getAllLaunches(){
     return Array.from(launches.values())
 }
 
+function addNewLaunch(launch){
+    lastFlightNumber++
+    launches.set(
+        lastFlightNumber,
+        Object.assign(launch,{
+            flightNumber: lastFlightNumber,
+            customer: ['NASA', 'NOAA'],
+            upcoming: true,
+            succes: true,
+        })
+    )
+}
+
 module.exports = {
     getAllLaunches,
+    addNewLaunch,
 }
--- NASA Project/server/src/routes/launches/launches_controller.js
+++ NASA Project/server/src/routes/launches/launches_controller.js
@@ -1,7 +1,7 @@
 const { getAllLaunches } = require('../../models/launches_model')
 
 function httpGetAllLaunches(request, response) {
-    return response.status(200).json(getAllLaunches)
+    return response.status(200).json(getAllLaunches())
 }
 
 module.exports = {
