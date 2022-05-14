fd8633cb6d8c3f1d2e03bbdb8e863266bb147c45
Fumigatus <alpcan237@gmail.com>
2022-05-14 20:22:15 +0300

    clustering

--- Performance Example/server.js
+++ Performance Example/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const cluster = require('cluster')
+
 const app = express()
 
 function delay(mSeconds){
@@ -10,15 +12,21 @@ function delay(mSeconds){
 
 
 app.get('/', (req, res) => {
-    res.send('Main Page')
+    res.send(`Main Page pid:${process.pid}`)
 })
 
 app.get('/timer', (req, res) => {
     delay(5000)
-    res.send('Timer Page')
+    res.send(`Timer Page pid:${process.pid}`)
 })
 
-
-app.listen(3000, () => {
-    console.log('listening port 3000')
-})
+if(cluster.isMaster){
+    console.log('Master process has been started.')
+    cluster.fork()
+    cluster.fork()
+} else{
+    console.log('Worker process has been started')
+    app.listen(3000, () => {
+        console.log(`pid: ${process.pid} listening port 3000`)
+    })
+}
