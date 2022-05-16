060e4aee5f4700933d280288b04534b94ec4887b
Fumigatus <alpcan237@gmail.com>
2022-05-16 19:03:53 +0300

    creating models from mongoose schemas

--- NASA Project/server/src/models/planet_mongo.js
+++ NASA Project/server/src/models/planet_mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose')
 
-const planetSchema = new mongoose.Schema({
+const planetsSchema = new mongoose.Schema({
     keplerName: {
         type: Number,
         required: true,
     }
 })
+
+module.exports = mongoose.model('Planet',planetsSchema)
