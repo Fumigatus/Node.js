439cbe70e017a8ff5fd3e12bd967bef7f832a091
Fumigatus <alpcan237@gmail.com>
2022-05-25 15:42:00 +0300

    versioning node apis

--- NASA Project/server/src/app.js
+++ NASA Project/server/src/app.js
@@ -2,9 +2,8 @@ const PATH = require('path')
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+const api = require('./routes/api_v1')
 
-const planetsRouter = require('./routes/planets/planets_router')
-const launchesRouter=require('./routes/launches/launcher_router')
 
 const app = express()
 
@@ -16,9 +15,8 @@ app.use(morgan('combined'))
 app.use(express.json())
 app.use(express.static(PATH.join(__dirname, '..', 'public')))
 
+app.use('/v1',api)
 
-app.use('/planets',planetsRouter)
-app.use('/launches',launchesRouter)
 app.get('/*', (request, response) => {
     response.sendFile(PATH.join(__dirname, '..', 'public', 'index.html'))
 })
