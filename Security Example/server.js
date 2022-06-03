3e4518807ebb53f2e0871ded28f77f2f6957ff41
Fumigatus <alpcan237@gmail.com>
2022-06-03 13:38:26 +0300

    serialize and deserialize

--- Security Example/server.js
+++ Security Example/server.js
@@ -1,11 +1,11 @@
-const fs = require('fs')
-const https = require('https')
-const path = require('path')
-const express = require('express')
-const helmet = require('helmet')
-const passport = require('passport')
-const { Strategy } = require('passport-google-oauth20')
-const cookieSession = require('cookie-session')
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const express = require('express');
+const helmet = require('helmet');
+const passport = require('passport');
+const { Strategy } = require('passport-google-oauth20');
+const cookieSession = require('cookie-session');
 require('dotenv').config()
 
 
@@ -14,30 +14,51 @@ const PORT = 3000
 const config = {
     CLIENT_ID: process.env.CLIENT_ID,
     CLIENT_SECRET: process.env.CLIENT_SECRET,
-    COKKIE_KEY_1: process.env.COKKIE_KEY_1,
-    COKKIE_KEY_2: process.env.COKKIE_KEY_2,
+    COOKIE_KEY_1: process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2: process.env.COOKIE_KEY_2,
 }
 
 const AUTH_OPTIONS = {
     callbackURL: '/auth/google/callback',
     clientID: config.CLIENT_ID,
-    clientSecret: config.CLIENT_SECRET
+    clientSecret: config.CLIENT_SECRET,
 }
 
 function verifyCallback(accessToken, refreshToken, profile, done) {
-    console.log('Google Profile:', profile)
-    done(null, profile)
+    console.log('Google profile', profile);
+    done(null, profile);
 }
+
+
+passport.use(new Strategy(AUTH_OPTIONS, verifyCallback));
+
+//save the session to cookie
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+})
+
+//read the session from to cookie
+passport.deserializeUser((id, done) => {
+    // database
+    // User.findById(id).then((user)=>{
+    //     done(null,user)
+    // }) //req.user
+    done(null, id)
+})
 
 const app = express()
 
 app.use(helmet())
+
 app.use(cookieSession({
     name: 'session',
     maxAge: 24 * 60 * 60 * 1000,
-    keys: [config.COKKIE_KEY_2, config.COKKIE_KEY_2]
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2],
 }))
-passport.use(new Strategy(AUTH_OPTIONS, verifyCallback))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 
 function checkLoggedIn(req, res, next) {
     const isLoggedIn = true //TODO: check with google account
@@ -52,20 +73,22 @@ function checkLoggedIn(req, res, next) {
 app.get('/auth/google',
     passport.authenticate('google', {
         scope: ['email'],
-    })
-)
+    }))
 
 app.get('/auth/logout', (req, res) => {
 
 })
 
-app.get('auth/google/callback', passport.authenticate('google', {
-    failureRedirect: '/failure',
-    successRedirect: '/',
-    session: false,
-}), (req, res) => {
-    console.log('Google callback')
-})
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/failure',
+        successRedirect: '/',
+        session: true,
+    }),
+    (req, res) => {
+        console.log('Google called us back!');
+    }
+)
 
 app.get('/secret', checkLoggedIn, (request, response) => {
     return response.send('Secret value')
@@ -81,7 +104,7 @@ app.get('/', (request, response) => {
 
 https.createServer({
     key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
+    cert: fs.readFileSync('cert.pem'),
 }, app).listen(PORT, () => {
-    console.log(`listening port ${PORT}`)
+    console.log(`Listening port ${PORT}`);
 })
