25af5576c590ae1a39a750009423befb8af36a06
Fumigatus <alpcan237@gmail.com>
2022-06-24 21:13:12 +0300

    connecting to sokcet.io

--- Socket Example (Multiplayer Pong)/public/javascripts/script.js
+++ Socket Example (Multiplayer Pong)/public/javascripts/script.js
@@ -1,8 +1,7 @@
 // Canvas Related 
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
-const socket = io('/pong');
-let isReferee = false;
+const socket = io('http://localhost:3000');
 let paddleIndex = 0;
 
 let width = 500;
@@ -12,8 +11,8 @@ let height = 700;
 let paddleHeight = 10;
 let paddleWidth = 50;
 let paddleDiff = 25;
-let paddleX = [ 225, 225 ];
-let trajectoryX = [ 0, 0 ];
+let paddleX = [225, 225];
+let trajectoryX = [0, 0];
 let playerMoved = false;
 
 // Ball
@@ -27,7 +26,7 @@ let speedY = 2;
 let speedX = 0;
 
 // Score for Both Players
-let score = [ 0, 0 ];
+let score = [0, 0];
 
 // Create Canvas Element
 function createCanvas() {
@@ -90,11 +89,6 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
-  socket.emit('ballMove', {
-    ballX,
-    ballY,
-    score,
-  });
 }
 
 // Adjust Ball Movement
@@ -105,11 +99,6 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
-  socket.emit('ballMove', {
-    ballX,
-    ballY,
-    score,
-  });
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -165,23 +154,18 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  if (isReferee) {
-    ballMove();
-    ballBoundaries();
-  }
+  ballMove();
   renderCanvas();
+  ballBoundaries();
   window.requestAnimationFrame(animate);
 }
 
-// Load Game, Reset Everything
-function loadGame() {
+// Start Game, Reset Everything
+function startGame() {
   createCanvas();
   renderIntro();
-  socket.emit('ready');
-}
 
-function startGame() {
-  paddleIndex = isReferee ? 0 : 1;
+  paddleIndex = 0;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
@@ -192,34 +176,11 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
-    socket.emit('paddleMove', {
-      xPosition: paddleX[paddleIndex],
-    });
     // Hide Cursor
     canvas.style.cursor = 'none';
   });
 }
 
 // On Load
-loadGame();
+startGame();
 
-socket.on('connect', () => {
-  console.log('Connected as...', socket.id);
-});
-
-socket.on('startGame', (refereeId) => {
-  console.log('Referee is', refereeId);
-
-  isReferee = socket.id === refereeId;
-  startGame();
-});
-
-socket.on('paddleMove', (paddleData) => {
-  // Toggle 1 into 0, and 0 into 1
-  const opponentPaddleIndex = 1 - paddleIndex;
-  paddleX[opponentPaddleIndex] = paddleData.xPosition;
-});
-
-socket.on('ballMove', (ballData) => {
-  ({ ballX, ballY, score } = ballData);
-});
