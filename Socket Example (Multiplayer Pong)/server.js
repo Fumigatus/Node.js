5ee46ea427674563240f9f5f5b53882d8706129c
Fumigatus <alpcan237@gmail.com>
2022-06-24 21:13:31 +0300

    Handling CORS

--- Socket Example (Multiplayer Pong)/server.js
+++ Socket Example (Multiplayer Pong)/server.js
@@ -1,10 +1,18 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+    }
+})
 
 const PORT = 3000
 
-server.listen(PORT)
-console.log(`listening port ${PORT}`)
+server.listen(PORT, () => {
+    console.log(`listening port: ${PORT}`)
+})
 
 io.on('connection', (socket) => {
     console.log('a user connected')
