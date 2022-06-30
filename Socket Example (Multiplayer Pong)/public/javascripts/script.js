2eb87181cbc69bfee428069707aded17f7d4c93f
Fumigatus <alpcan237@gmail.com>
2022-06-30 22:54:57 +0300

    ball movement

--- Socket Example (Multiplayer Pong)/public/javascripts/script.js
+++ Socket Example (Multiplayer Pong)/public/javascripts/script.js
@@ -90,6 +90,11 @@ function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score,
+  })
 }
 
 // Adjust Ball Movement
@@ -100,6 +105,11 @@ function ballMove() {
   if (playerMoved) {
     ballX += speedX;
   }
+  socket.emit('ballMove', {
+    ballX,
+    ballY,
+    score,
+  })
 }
 
 // Determine What Ball Bounces Off, Score Points, Reset Ball
@@ -155,8 +165,10 @@ function ballBoundaries() {
 
 // Called Every Frame
 function animate() {
-  ballMove();
-  renderCanvas();
+  if (isReferee) {
+    ballMove();
+    renderCanvas();
+  }
   ballBoundaries();
   window.requestAnimationFrame(animate);
 }
@@ -205,4 +217,8 @@ socket.on('startGame', (refereeId) => {
 socket.on('paddleMove', (paddleData) => {
   const oppenentPaddleIndex = 1 - paddleIndex
   paddleX[oppenentPaddleIndex] = paddleData.xposition
+})
+
+socket.on('ballMove', (ballData) => {
+  ({ ballX, ballY, score } = ballData)
 })
