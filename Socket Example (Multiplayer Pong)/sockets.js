4306800b57dff0d6b36b7b1a1fc1555f188eb225
Fumigatus <alpcan237@gmail.com>
2022-07-02 17:23:04 +0300

    room logic

--- Socket Example (Multiplayer Pong)/sockets.js
+++ Socket Example (Multiplayer Pong)/sockets.js
@@ -2,27 +2,31 @@ let readyPlayerCount = 0
 
 function listen(io) {
     const pongNamespace=io.of('/pong')
+    let room
     io.on('connection', (socket) => {
         console.log(`a user connecteed as id: ${socket.id}`)
 
         socket.on('ready', () => {
+            room ='room'+Math.floor(readyPlayerCount/2)
+            socket.join(room)
             readyPlayerCount++;
-            console.log(`Client ${socket.id} is ready`)
+            console.log(`Client ${socket.id} is ready room id:${room}`)
             if (readyPlayerCount % 2 === 0) {
-                pongNamespace.emit('startGame', socket.id)
+                pongNamespace.in(room).emit('startGame', socket.id)
             }
         })
 
         socket.on('paddleMove', (paddleData) => {
-            socket.broadcast.emit('paddleMove', paddleData)
+            socket.to(room).emit('paddleMove', paddleData)
         })
 
         socket.on('ballMove', (ballData) => {
-            socket.broadcast.emit('ballMove', ballData)
+            socket.to(room).emit('ballMove', ballData)
         })
 
         socket.on('disconnect', (reason) => {
             console.log(`Client ${socket.id} has been disconnected: ${reason}`)
+            socket.leave(room)
         })
     })
 }
